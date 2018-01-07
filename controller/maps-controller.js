ee72391bc67bd569225a051a362e4037654581e2
kate brenner <kate.e.brenner@gmail.com>
2018-01-06 21:42:25 -0500

    working on create functionality, and also added features to index page

--- controller/maps-controller.js
+++ controller/maps-controller.js
@@ -1,4 +1,5 @@
  const mapModel = require('../model/mapModel');
+ const axios = require('axios');
  const mapController = {}
 
 mapController.index = (req, res) => {
@@ -13,5 +14,49 @@ mapController.index = (req, res) => {
     res.status(500).json(err)
   })
 }
+
+
+mapController.new = (req, res) => {
+  mapModel.findall()
+    .then(locations => {
+      res.render('map/new', { locations: locations })
+      console.log(locations)
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+ //    axios({
+ //    method: 'get',
+ //    //base url zipcode is now dynamic and is looking for input with a name of zipcode
+ //    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.address}&apikey=AIzaSyCwGVukC1LFAwcw415VQCJpNb3n4V8VYUk`
+ //  })
+ //  .then(data => {
+ //    console.log(data.data.results[0].geometry.location.lng)
+ //    res.render('map/new', {
+ //    lat: data.data.results[0].geometry.location.lat,
+ //    lng: data.data.results[0].geometry.location.lng
+
+ //  })
+ // })
+
+};
+
+
+// mapController.create = (req, res) => {
+//   mapModel.create({
+//       name: req.body.name,
+//       address: req.body.address,
+//       lat: req.body.lat,
+//       lng: req.body.lng,
+//       type: req.body.type
+//     })
+//     .then(locations => {
+//       res.redirect(`/locations/${locations.id}`)
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// };
+
 
  module.exports = mapController
--- routes/map-routes.js
+++ routes/map-routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const controller = require('../controller/maps-controller');
 
 router.get('/', controller.index);
+router.get('/new', controller.new)
 
 module.exports = router;
