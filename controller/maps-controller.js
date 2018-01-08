657593b8fe8cae8d7c22f2feaf8a3986ca56ab57
kate brenner <kate.e.brenner@gmail.com>
2018-01-08 08:55:51 -0500

    added controller function from create branch

--- controller/maps-controller.js
+++ controller/maps-controller.js
@@ -1,5 +1,48 @@
  const mapModel = require('../model/mapModel');
+ const axios = require('axios');
  const mapController = {}
+
+mapController.index = (req, res) => {
+  mapModel.findall().then( locations => {
+    console.log(locations);
+    console.log(locations[0].name);
+    res.render('map/index', {
+      locations: locations
+    });
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  })
+}
+
+
+mapController.new = (req, res) => {
+  mapModel.findall()
+    .then(locations => {
+      res.render('map/new', { locations: locations })
+      console.log(locations)
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+};
+
+
+mapController.create = (req, res) => {
+  mapModel.create({
+      name: req.body.name,
+      address: req.body.address,
+      lat: req.body.lat,
+      lng: req.body.lng,
+      type: req.body.type
+    })
+    .then(locations => {
+      res.redirect(`/locations/${locations.id}`)
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+};
 
 
  module.exports = mapController
